84a30e329372e04b774d0ebacccb8bb007b7e554
kevinanm <kevin.moreno@openmarket.com>
2021-06-29 09:57:21 -0500

    Spring Initializr

---
+++ src/test/java/kevin/springframework/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kevin.springframework.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
